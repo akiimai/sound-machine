3cdb7de535b47c0c5907c3622c1b64e9e9f84585
Aki Imai <31362969+akiimai@users.noreply.github.com>
2019-03-09 11:52:13 -0800

    update

--- src/components/button.js
+++ src/components/button.js
@@ -4,12 +4,10 @@ class Button extends Component {
   render() {
     return(
       <div
-      // Add additional attributes here
         className='button'
         onClick={this.props.playSound}
         id={this.props.sound}
       >
-
         <p>{this.props.sound}</p>
 
       </div>
